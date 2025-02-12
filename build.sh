6182bad2919969deac68e371546e0041f0b4bcab
Motlakhov <dmitrijmotlahov@gmail.com>
2025-02-13 02:27:09 +0300

    first deploy

--- build.sh
+++ build.sh
@@ -3,9 +3,6 @@
 curl -LsSf https://astral.sh/uv/install.sh | sh
 source $HOME/.local/bin/env
 
-# Добавить каталог poetry в PATH
-export PATH="/opt/render/project/poetry/bin:$PATH"
-
 # здесь добавьте все необходимые команды для установки вашего проекта
 # команду установки зависимостей, сборки статики, применения миграций и другие
 make install && make collectstatic && make migrate
